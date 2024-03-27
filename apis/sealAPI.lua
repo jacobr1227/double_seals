87bda85e535dc5aacc83050f4245d906965f9183
jacobr1227 <jacobrogers1227@gmail.com>
2024-03-27 18:14:44 -0500

    Certificate Joker and Shops work

--- apis/sealAPI.lua
+++ apis/sealAPI.lua
@@ -1,13 +1,17 @@
 local mod_id = "sealAPI"
 local mod_name = "Seal API"
-local mod_version = "0.7"
+local mod_version = "0.85"
 local mod_author = "jacobr1227"
 
+local loc_list = {}
+
 -- Seal_id MUST contain no spaces or lua-unsafe characters.
--- Image name in assets/ must match the seal_id, but lowercase and adding _seal at the end. 
-function add_seal(seal_id, label, desc)
+-- Image name in assets/ must match the seal_id, but lowercase and adding _seal at the end.
+-- Note: function code will need to be added manually. Refer to https://github.com/jacobr1227/double_seals/double_seals.lua for examples
+function add_seal(seal_id, label, color, shader, desc)
     -- Prepare the data contents, add to the pools.
     local loc_id = string.lower(seal_id) .. "_seal"
+    table.insert(loc_list, loc_id)
     local data = {}
     data.set = 'Seal'
     data.discovered = false
@@ -15,6 +19,7 @@ function add_seal(seal_id, label, desc)
     data.order = #G.P_CENTER_POOLS.Seal + 1
     G.P_SEALS[seal_id] = data
     table.insert(G.P_CENTER_POOLS['Seal'], data)
+    desc.name = label
 
     -- Parse localization text.
     local newSealText = {
@@ -47,6 +52,14 @@ function add_seal(seal_id, label, desc)
         y = 0
     })
 
+    if shader then
+        add_shader(seal_id, shader)
+    end
+    -- Inserting colors for badges/etc.
+    local color_string = string.format([[purple_seal = G.C.PURPLE,
+    %s = %s]], loc_id, "G.C." .. string.upper(color) .. ',')
+    inject("functions/UI_definitions.lua", "get_badge_colour", "purple_seal = G.C.PURPLE,", color_string)
+
     -- I like sorted tables :)
     for k, v in pairs(G.P_SEALS) do
         table.sort(v, function(a, b)
@@ -58,7 +71,67 @@ function add_seal(seal_id, label, desc)
     end)
 end
 
--- Debug tools for table data if you need it.
+function inject_overrides()
+    local info_queues = [[local info_queue = {}
+    if first_pass and not (_c.set == 'Edition') and badges then
+        for k, v in ipairs(badges) do
+            ]]
+    if #loc_list > 0 then
+        for i = 1, #loc_list do
+            info_queues = info_queues ..
+                              string.format(
+                    [[if v == '%s' then info_queue[#info_queue + 1] = {key = '%s', set = 'Other'} end
+            ]], loc_list[i], loc_list[i])
+        end
+        info_queues = info_queues .. [[end
+    end]]
+        inject("functions/common_events.lua", "generate_card_ui", "local info_queue = {}", info_queues)
+    end
+    local fun_name = "Card:open"
+    local file_name = "card.lua"
+    local to_replace = "local seal_type = pseudorandom"
+    local replacement = [[local seal_type = pseudorandom(pseudoseed('stdsealtype'..G.GAME.round_resets.ante), 1, #G.P_CENTER_POOLS['Seal'])
+        local sealName
+            for k, v in pairs(G.P_SEALS) do
+                if v.order == seal_type then 
+                    sealName = k
+                    card:set_seal(sealName)
+                end
+            end--]]
+    inject(file_name, fun_name, "card:set_seal", "eat")
+    inject(file_name, fun_name, to_replace, replacement)
+end
+
+function add_shader(seal_id, shader)
+    local valid_shader = {
+        dissolve = true,
+        voucher = true,
+        vortex = true,
+        negative = true,
+        holo = true,
+        foil = true,
+        debuff = true,
+        polychrome = true,
+        hologram = true,
+        played = true
+    }
+    if valid_shader[shader] ~= nil then
+        local replacement = string.format([[if self.seal then
+        if self.seal == '%s' then
+            G.shared_seals[self.seal].role.draw_major = self
+            G.shared_seals[self.seal]:draw_shader('dissolve', nil, nil, nil, self.children.center)
+            G.shared_seals[self.seal]:draw_shader('%s', nil, self.ARGS.send_to_shader, nil, self.children.center) 
+        end
+    end]], seal_id, shader)
+        injectTail("card.lua", "Card:draw", replacement)
+    else
+        sendDebugMessage("Error: invalid shader.")
+    end
+end
+
+-- Debug tools for various purposes.
+
+--Dumps table data to the caller
 function dump(o)
     if o == nil then
         return "nil."
@@ -74,5 +147,23 @@ function dump(o)
         return s .. '} '
     else
         return tostring(o)
+    end
+end
+
+--Checks how many potential replacements would occur with an injection. Used to check if inject targets are valid.
+function inject_checker(path, function_name, to_replace, replacement)
+    local function_body = extractFunctionBody(path, function_name)
+    local modified_function_code, num_replacements = function_body:gsub(to_replace, replacement)
+    return num_replacements
+end
+
+function eat(waste, this)
+    --This function intentionally does absolutely nothing.
+    --It is used as filler in replacements to fix inject()'s faults
+    if waste then
+        sendDebugMessage(waste)
+    end
+    if this then
+        sendDebugMessage("This!")
     end
 end
--- mods/double_seals.lua
+++ mods/double_seals.lua
@@ -2,43 +2,44 @@
 Authored March 2024, Jacob Rogers
 Core functions inspired by/modified from AxUtil @ https://github.com/AxBolduc/GreenSeal/blob/main/Util.lua]] mod_id =
     "double_seals"
+
+if (sendDebugMessage == nil) then
+    sendDebugMessage = function(_)
+    end
+end
+
 table.insert(mods, {
     mod_id = mod_id,
     name = "Double Seals",
     author = "jacobr1227",
-    version = "v0.8",
+    version = "v0.85",
     description = {"Adds new Double versions of the 4 base game seals\n as well as all my other seal mods."},
     enabled = true,
     on_enable = function()
-        add_seal("DoubleRed", "Double Red Seal", {
-            name = "Double Red Seal",
-            text = {"Retrigger this", "card {C:attention}2{} times"}
-        })
-        add_seal("DoubleGold", "Double Gold Seal", {
-            name = "Double Gold Seal",
+        add_seal("DoubleGold", "Double Gold Seal", "gold", "voucher", {
             text = {"Earn {C:money}$6{} when this", "card is played", "and scores"}
         })
-        add_seal("DoubleBlue", "Double Blue Seal", {
-            name = "Double Blue Seal",
+        add_seal("DoubleRed", "Double Red Seal", "red", nil, {
+            text = {"Retrigger this", "card {C:attention}2{} times"}
+        })
+        add_seal("DoubleBlue", "Double Blue Seal", "blue", nil, {
             text = {"Creates 2 {C:planet}Planet{} cards", "if this card is {C:attention}held{} in",
                     "hand at end of round", "{C:inactive}(Must have room)"}
         })
-        add_seal("DoublePurple", "Double Purple Seal", {
+        add_seal("DoublePurple", "Double Purple Seal", "purple", nil, {
             name = "Double Purple Seal",
             text = {"Creates 2 {C:tarot}Tarot{} cards", "when {C:attention}discarded.", "{C:inactive}(Must have room)"}
         })
         if mods["Orange Seals"] then
             sendDebugMessage("Found orange seals!")
-            add_seal("DoubleOrange", "Double Orange Seal", {
-                name = "Double Orange Seal",
+            add_seal("DoubleOrange", "Double Orange Seal", "orange", nil, {
                 text = {"Randomly {C:attention}enhances{} 2 cards", "when {C:attention}discarded{}",
                         "{C:inactive}(Can overwrite enhancements)"}
             })
         end
         if mods["Silver Seals"] then
             sendDebugMessage("Found silver seals!")
-            add_seal("DoubleSilver", "Double Silver Seal", {
-                name = "Double Silver Seal",
+            add_seal("DoubleSilver", "Double Silver Seal", "grey", "polychrome", {
                 text = {"Creates 2 {C:spectral}Spectral{} cards", "if this card is {C:attention}held{} in",
                         "hand at end of round"}
             })
@@ -83,7 +84,7 @@ table.insert(mods, {
                     })
                 end
                 if self.seal == 'DoubleOrange' then
-                    --Enhance two different cards in hand, randomly.
+                    -- Enhance two different cards in hand, randomly.
                 end
             end
             return fromRef
@@ -157,102 +158,24 @@ table.insert(mods, {
         local file_name = "functions/common_events.lua"
         local fun_name = "eval_card"
         inject(file_name, fun_name, to_replace, replacement)
-
-        -- Double Gold shader [[Move to API??]]
-        local replacement = [[if self.seal then
-                    if self.seal == 'DoubleGold' then
-                        G.shared_seals[self.seal].role.draw_major = self
-                        G.shared_seals[self.seal]:draw_shader('dissolve', nil, nil, nil, self.children.center)
-                        G.shared_seals[self.seal]:draw_shader('voucher', nil, self.ARGS.send_to_shader, nil, self.children.center) 
-                    end
-                end]]
-        local fun_name = "Card:draw"
-        local file_name = "card.lua"
-        injectTail(file_name, fun_name, replacement)
-
-        -- Color additions for UI badges [[Move to API?]]
-        local to_replace = "purple_seal = G.C.PURPLE,"
-        local replacement = [[purple_seal = G.C.PURPLE,
-            doublered_seal = G.C.RED,
-            doublegold_seal = G.C.GOLD,
-            doubleblue_seal = G.C.BLUE,
-            doublepurple_seal = G.C.PURPLE,
-            doubleorange_seal = G.C.ORANGE,
-            doublesilver_seal = G.C.GREY,]]
-        local file_name = "functions/UI_definitions.lua"
-        local fun_name = "get_badge_colour"
-        inject(file_name, fun_name, to_replace, replacement)
-
-        -- Color additions for localization items [[Move to API??]]
-        local to_replace = [[G.ARGS.LOC_COLOURS = G.ARGS.LOC_COLOURS or {
-            red = G.C.RED,]]
-        local replacement = [[G.ARGS.LOC_COLOURS = G.ARGS.LOC_COLOURS or {
-            red = G.C.RED,
-            doublered = G.C.RED,
-            doublegold = G.C.GOLD,
-            doubleblue = G.C.BLUE,
-            doublepurple = G.C.PURPLE,
-            doubleorange = G.C.ORANGE,
-            doublesilver = G.C.GREY,]]
-        local file_name = "functions/misc_functions.lua"
-        local fun_name = "loc_colour"
-        inject(file_name, fun_name, to_replace, replacement)
-
-        -- Card UI updater for badges [[Move to API??]]
-        inject("functions/UI_definitions.lua", "create_UIBox_your_collection_seals", "card_limit = 4", "card_limit = 8")
-        inject("functions/common_events.lua", "generate_card_ui", "local info_queue = {}", [[local info_queue = {}
-        if first_pass and not (_c.set == 'Edition') and badges then
-            for k, v in ipairs(badges) do
-                if v == 'doublered_seal' then info_queue[#info_queue + 1] = {key = 'doublered_seal', set = 'Other'} end
-                if v == 'doubleblue_seal' then info_queue[#info_queue + 1] = {key = 'doubleblue_seal', set = 'Other'} end
-                if v == 'doublegold_seal' then info_queue[#info_queue + 1] = {key = 'doublegold_seal', set = 'Other'} end
-                if v == 'doublepurple_seal' then info_queue[#info_queue + 1] = {key = 'doublepurple_seal', set = 'Other'} end
-                if v == 'doublesilver_seal' then info_queue[#info_queue + 1] = {key = 'doublesilver_seal', set = 'Other'} end
-                if v == 'doubleorange_seal' then info_queue[#info_queue + 1] = {key = 'doubleorange_seal', set = 'Other'} end
-            end
-        end
-        sendDebugMessage('Hi!')]])
-        -- localize{type = 'other', key = _c.key, nodes = desc_nodes, vars = specific_vars}
-        -- DoubleRed example: type = 'other', key = 'doublered_seal'
-        -- loc_target = name: Double Red Seal, text: (desc)
-
-        -- Booster pack seal appearance [BUGGED!!]
-        local to_replace = [[if seal_poll > 1 - 0.02*seal_rate then
-            local seal_type = pseudorandom(pseudoseed('stdsealtype'..G.GAME.round_resets.ante))
-            if seal_type > 0.75 then card:set_seal('Red')
-            elseif seal_type > 0.5 then card:set_seal('Blue')
-            elseif seal_type > 0.25 then card:set_seal('Gold')
-            else card:set_seal('Purple')
-            end
-            end]]
-        local replacement = [[if seal_poll > 1 - 0.02*seal_rate then
-            local seal_type = pseudorandom(pseudoseed('stdsealtype'..G.GAME.round_resets.ante), 1, #G.P_CENTER_POOLS['Seal'])
-            local sealName
-            sendDebugMessage("Man you must really love boosters...")
+        
+        -- Certificate Joker seal appearance
+        local to_replace = "local seal_type = pseudorandom"
+        local replacement = [[local seal_type = pseudorandom(pseudoseed('certsl'), 1, #G.P_CENTER_POOLS['Seal'])
+        local sealName
             for k, v in pairs(G.P_SEALS) do
-                if v.order == seal_type then sealName = k end
+                if v.order == seal_type then 
+                    sealName = k
+                    _card:set_seal(sealName, true)
+                end
             end
-            card:set_seal(sealName)]]
-        local fun_name = "Card:open"
+            local throwaway = pseudorandom]]
         local file_name = "card.lua"
-        inject(file_name, fun_name, to_replace, replacement)
-
-        -- Certificate Joker seal appearance [BUGGED!!]
-        local to_replace = [[local seal_type = pseudorandom(pseudoseed('certsl'))
-            if seal_type > 0.75 then _card:set_seal('Red', true)
-            elseif seal_type > 0.5 then _card:set_seal('Blue', true)
-            elseif seal_type > 0.25 then _card:set_seal('Gold', true)
-            else _card:set_seal('Purple', true)]]
-        local replacement = [[local seal_type = pseudorandom(pseudoseed('certsl'))
-            local sealName
-            sendDebugMessage("Certified!")
-            for k, v in pairs(G.P_SEALS) do
-                if v.order == seal_type then sealName = k end
-            end
-            card:set_seal(sealName)]]
         local fun_name = "Card:calculate_joker"
+        inject(file_name, fun_name, "_card:set_seal", "eat")
         inject(file_name, fun_name, to_replace, replacement)
 
+        inject_overrides()
         sendDebugMessage("Double Seals enabled!")
     end,
     on_disable = function()
